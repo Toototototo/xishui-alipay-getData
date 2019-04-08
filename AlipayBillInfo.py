4e94bb991ead912238e3d6006bca018744d6dedc
dengfeng <953841731@qq.com>
2019-04-08 18:05:31 +0800

    [FIX]

--- AlipayBillInfo.py
+++ AlipayBillInfo.py
@@ -16,7 +16,9 @@ from BaiduAIP import BaiduAIP
 # 登录 url
 Login_Url = "https://auth.alipay.com/login/index.htm?goto=https%3A%2F%2Fwww.alipay.com%2F"
 # 账单 url
-Bill_Url = "https://consumeprod.alipay.com/record/standard.htm"
+Bill_Url = "https://consumeprod.alipay.com/record/advanced.htm"
+# 账单下载 url
+Download_Url = "https://consumeprod.alipay.com:443/record/download.htm?_input_charset=utf-8&suffix=csv&endDate=%(endDate)&dateRange=customDate&pageNum=1&beginDate=%(beginDate)&dateType=createDate&fundFlow=all&beginTime=00%3A00&endTime=24%3A00&tradeType=ALL&status=all"
 
 # 登录用户名和密码
 USERNMAE = ""
@@ -48,6 +50,8 @@ class Alipay_Bill_Info(object):
         cookies: 持久化访问
         info_list: 存储账单信息的列表
         '''
+        self.sel = None
+        self.cookies = None
         self.headers = headers
         # 初始化用户名和密码
         self.user = user
@@ -69,56 +73,61 @@ class Alipay_Bill_Info(object):
     def get_cookies(self):
         '''获取 cookies'''
 
-        # 查看当前session是否包含cookie
-        cookie = self.session.cookies
+        if self.cookies is not None:
+            cookies_dict = {}
+            for cookie in self.cookies:
+                if "name" in cookie and "value" in cookie:
+                    cookies_dict[cookie["name"]] = cookie["value"]
+
+            return cookies_dict
 
         # 初始化浏览器对象
-        sel = webdriver.PhantomJS()
-        sel.maximize_window()
-        sel.get(Login_Url)
-        sel.implicitly_wait(3)
+        self.sel = webdriver.PhantomJS()
+        self.sel.maximize_window()
+        self.sel.get(Login_Url)
+        self.sel.implicitly_wait(3)
 
         # 找到账号密码登录
-        login_interface = sel.find_element_by_xpath("//*[@id='J-loginMethod-tabs']/li[2]")
+        login_interface = self.sel.find_element_by_xpath("//*[@id='J-loginMethod-tabs']/li[2]")
         login_interface.click()  # 点击
         # 截图查看
-        # snap(sel)
+        snap(self.sel)
         time.sleep(2)
 
         # 找到用户名字输入框
-        uname = sel.find_element_by_id("J-input-user")
+        uname = self.sel.find_element_by_id("J-input-user")
         uname.clear()
         print("正在输入账号.....")
         self.wait_input(uname, self.user)
         time.sleep(1)
 
         # 找到密码输入框
-        upass = sel.find_element_by_id("password_rsainput")
+        upass = self.sel.find_element_by_id("password_rsainput")
         upass.clear()
         print("正在输入密码....")
         self.wait_input(upass, self.passwd)
 
         # 判断是否需要输入验证码
-        check_code = sel.find_element_by_id("J-checkcode")
+        check_code = self.sel.find_element_by_id("J-checkcode")
         check_class = check_code.get_attribute("class")
         print(check_class, "fn_hidden" not in check_class)
         if "fn-hide" not in check_class:
             # 需要输入验证码
-            snap(sel)
+            snap(self.sel)
             i = 0
             code = ''
             while i < 10:
                 i+=1
                 print('code', code)
                 if code == '': # 第一次
-                    ucheckimg = sel.find_element_by_id("J-checkcode-img")
+                    ucheckimg = self.sel.find_element_by_id("J-checkcode-img")
                     picture_src = ucheckimg.get_attribute("src")
                     self.baiduAIP.picture_path = picture_src
                     code = self.baiduAIP.get_code()
                 elif code == 'do again':  # 未成功识别
-                    ucheckimg = sel.find_element_by_id("J-checkcode-img")
+                    ucheckimg = self.sel.find_element_by_id("J-checkcode-img")
                     ucheckimg.click()  # 点击验证码，重新获取验证码
-                    ucheckimg = sel.find_element_by_id("J-checkcode-img")
+                    ucheckimg = self.sel.find_element_by_id("J-checkcode-img")
                     picture_src = ucheckimg.get_attribute("src")
                     self.baiduAIP.picture_path = picture_src
                     code = self.baiduAIP.get_code()
@@ -126,34 +135,44 @@ class Alipay_Bill_Info(object):
                     break
 
             if code != 'do again':
-                ucheck = sel.find_element_by_id("J-input-checkcode")
+                ucheck = self.sel.find_element_by_id("J-input-checkcode")
                 ucheck.clear()
                 print('正在输入验证码...')
                 self.wait_input(ucheck, code)
+            else:
+                print('验证码填写错误...')
+                return
 
-        snap(sel)
+        snap(self.sel)
         # 找到登录按钮
-        # button = sel.find_element_by_id("J-login-btn")
-        # time.sleep(1)
-        # button.click()
+        button = self.sel.find_element_by_id("J-login-btn")
+        time.sleep(1)
+        button.click()
 
         # 跳转到账单页面
-        # print("正在跳转页面....")
-        # sel.get(Bill_Url)
-        # sel.implicitly_wait(3)
+        print("正在跳转页面....")
+        self.sel.get(Bill_Url)
+        self.sel.implicitly_wait(3)
 
-        # snap(sel)
+        snap(self.sel)
         # 获取 cookies 并转换为字典类型
-        cookies = sel.get_cookies()
+        self.cookies = self.sel.get_cookies()
         cookies_dict = {}
-        for cookie in cookies:
+        for cookie in self.cookies:
             if "name" in cookie and "value" in cookie:
                 cookies_dict[cookie["name"]] = cookie["value"]
 
+        # 关闭浏览器
+        # self.sel.close()
+
         return cookies_dict
 
-        # 关闭浏览器
-        sel.close()
+    def quite(self):
+        """退出"""
+        self.cookies = None
+        self.sel.close()
+        self.sel = None
+        self.session.cookies.update(None)
 
     def set_cookies(self):
         '''将获取到的 cookies 加入 session'''
@@ -172,21 +191,24 @@ class Alipay_Bill_Info(object):
             return True
         else:
             # cookie 失效
-            self.session.cookies.update(None)
+            self.quite()
             return False
 
     def get_data(self):
         """
-        利用 bs4 库解析 html
+        # 利用 bs4 库解析 html
+        通过下载数据包，解析csv文件读取账单信息
         并抓取数据，
         数据以字典格式保存在列表里
         """
         status = self.login_status()
-        if status:
+        if status and self.sel is not None:
+            pass
+            download_url = self.sel
             html = self.session.get(Bill_Url).text
             soup = BeautifulSoup(html, "lxml")
-            # 抓取前五个交易记录
-            trades = soup.find_all("tr", class_="J-item ")[:5]
+            # 抓取前当前页所有交易记录
+            trades = soup.find_all("tr", class_="J-item")
 
             for trade in trades:
                 # 做一个 try except 避免异常中断
@@ -201,7 +223,6 @@ class Alipay_Bill_Info(object):
                         dict(time=time, amount=amount, code=code))
                 except:
                     self.info_list.append({"error": "出现错误"})
-
         else:
-            self.info_list.append({"error": "出现错误"})
+            self.info_list.append({"error": "登录失败，出现错误"})
         return self.info_list
--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from AlipayBillInfo import Alipay_Bill_Info, HEADERS, USERNMAE, PASSWD
 
 if __name__ == '__main__':
     alipay = Alipay_Bill_Info(headers=HEADERS, user=USERNMAE, passwd=PASSWD)
-    # alipay.get_cookies()
+    alipay.get_cookies()
+    print(alipay.__dict__)
